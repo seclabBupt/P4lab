8181b27ad6fc2e2efc6716a8a7dda16db305b390
josh-sayisi <65650165+josh-sayisi@users.noreply.github.com>
2023-05-07 17:13:27 +0800

    Create rec.py

---
+++ ZZH/Cache/re_h1/rec.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+import argparse
+import sys
+import socket
+import random
+import struct
+
+from scapy.all import *
+from scapy.all import sendp, send, get_if_list, get_if_hwaddr
+from scapy.all import Packet
+from scapy.all import Ether, IP, UDP, TCP
+
+def get_if():
+    ifs=get_if_list()
+    iface=None
+    for i in get_if_list():
+        if "eth0" in i:
+            iface=i
+            break
+    if not iface:
+        print("Cannot find eth0 interface")
+        exit(1)
+    return iface
+
+
+def main():
+
+    iface = get_if()
+
+    print("sniffing")
+
+    sniff(iface=iface, prn=lambda p: p.show())
+
+
+if __name__ == '__main__':
+    main()
