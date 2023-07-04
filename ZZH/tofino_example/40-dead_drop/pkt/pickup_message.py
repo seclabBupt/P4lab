63bf83dde0100d419d522850d4f5f3756d99369f
josh-sayisi <65650165+josh-sayisi@users.noreply.github.com>
2023-07-04 21:10:39 +0800

    Add files via upload

---
+++ ZZH/tofino_example/40-dead_drop/pkt/pickup_message.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+
+import os
+
+if os.getuid() !=0:
+    print """
+ERROR: This script requires root privileges. 
+       Use 'sudo' to run it.
+"""
+    quit()
+
+from scapy.all import *
+
+class DeadDrop(Packet):
+    name = "Spy Dead-Drop"
+    fields_desc = [ ShortField("box_num",   0),
+                    ShortField("box_op",    0),
+                    ShortField("data_dest", 511),
+                    IntField("box_data",    0xDEADBABE)
+                  ]
+
+bind_layers(Ether, DeadDrop, type=0xDEAD)
+
+sendp(Ether()/DeadDrop(box_num=5, box_op=1), iface="veth3")
